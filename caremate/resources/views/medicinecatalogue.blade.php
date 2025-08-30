544d78667d6d645f406382a2b072c986fbe691e0
Sarah Chowdhury <sarah.chowdhury403@gmail.com>
2025-08-30 23:19:38 +0600

    feat: show medicine list on medicine catalogue page

--- caremate/resources/views/medicinecatalogue.blade.php
+++ caremate/resources/views/medicinecatalogue.blade.php
@@ -1,12 +1,14 @@
 @extends('layout')
 
 @section('content')
-<h1>medicine catalogue</h1>
-<div class="grid !grid-cols-3 grid-flow-col gap-4">
-    @for ($i=0; $i < 50;$i++)
-    <div class="bg-white rounded-lg shadow-xl">
-        tent
+<div class="container mx-auto p-8">
+<h1 class=" text-4xl">medicine catalogue</h1>
+    <div class="grid grid-cols-3 grid-flow-row gap-6 px-6 py-6 items-center font-sans w-full">
+        @foreach ($medicines as $medicine)
+            <div class="flex items-center bg-white rounded-lg shadow-xl w-full h-24 text-lg text-center">
+                {{$medicine['common_name']}}
+            </div>
+        @endforeach
     </div>
-    @endfor
 </div>
 @endsection
--- caremate/routes/web.php
+++ caremate/routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\MedicineCatalogueController;
+use App\Models\MedicineCatalogue;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -14,10 +16,5 @@ Route::get('/invoice', function () {
     return view('invoice');
 });
 
-Route::get('/medicinecatalogue', function () {
-    return view('medicinecatalogue');
-});
+Route::get('/medicinecatalogue', [MedicineCatalogueController::class, 'index']);
 
-Route::get('/tailwind', function () {
-    return view('tailwindtest');
-});
